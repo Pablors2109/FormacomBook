2c25bd68a8c250a5ce98ead9b515c772e2637565
Pablors2109 <mrpablo219@gmail.com>
2025-01-16 16:52:30 +0100

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" placeholder="nombre" required placeholder>
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email" required placeholder>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" placeholder="Contraseña">
+        <input type="submit" value="Registrarse">
+    </form>
+
+</body>
+</html>
